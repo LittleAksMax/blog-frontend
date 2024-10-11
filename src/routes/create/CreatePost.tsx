15ea66279402e055ecc5e0b502fe5837acf4c57e
David R <david2005.rosental@gmail.com>
2024-10-11 22:03:03 +0100

    Added authentication guards to posts editor and creator

--- src/routes/create/CreatePost.tsx
+++ src/routes/create/CreatePost.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react';
 import Page from '../../components/common/page/Page';
+import { useAuth } from '../../contexts/auth';
+import { Navigate } from 'react-router-dom';
 
 interface CreatePostProps {}
 
 const CreatePost: FC<CreatePostProps> = () => {
+  const auth = useAuth();
+
+  // if not logged in, we are not authorised to view this page
+  if (!auth.user) {
+    alert('Please log in to access this page.');
+    return <Navigate to="/" />;
+  }
+
   return (
     <Page>
       <h1>Create Page</h1>
--- src/routes/update/PostEditor.tsx
+++ src/routes/update/PostEditor.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react';
 import Page from '../../components/common/page/Page';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/auth';
 
 interface PostEditorProps {}
 
 const PostEditor: FC<PostEditorProps> = () => {
+  const auth = useAuth();
+
+  // if not logged in, we are not authorised to view this page
+  if (!auth.user) {
+    alert('Please log in to access this page.');
+    return <Navigate to="/" />;
+  }
+
   return (
     <Page>
       <h1>Update Page</h1>
