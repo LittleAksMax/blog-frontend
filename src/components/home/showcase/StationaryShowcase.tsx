03dd3caa372fe7d6033033103e06a316be8bf832
David R <david2005.rosental@gmail.com>
2024-10-11 21:56:56 +0100

    Fixed problem with string interpolation by restricting the values of the number of columns in the stationary showcase to be in [1,5] and then creating const strings to return

--- src/components/home/showcase/StationaryShowcase.tsx
+++ src/components/home/showcase/StationaryShowcase.tsx
@@ -2,6 +2,29 @@ import { FC, useMemo } from 'react';
 import { ShowcaseProps } from './common';
 import ShowcaseItem from './showcaseItem/ShowcaseItem';
 
+const COLS_1 = 'w-full';
+const COLS_2 = 'w-full md:w-1/2';
+const COLS_3 = 'w-full md:w-1/3';
+const COLS_4 = 'w-full md:w-1/4';
+const COLS_5 = 'w-full md:w-1/5';
+
+const dynamicClassName = (columns: 1 | 2 | 3 | 4 | 5): string => {
+  switch (columns) {
+    case 1:
+      return COLS_1;
+    case 2:
+      return COLS_2;
+    case 3:
+      return COLS_3;
+    case 4:
+      return COLS_4;
+    case 5:
+      return COLS_5;
+    default:
+      throw new Error(`Invalid column value: ${columns}`);
+  }
+};
+
 interface StationaryShowcaseProps extends ShowcaseProps {
   columns: 1 | 2 | 3 | 4 | 5; // just some values
 }
@@ -16,8 +39,7 @@ const StationaryShowcase: FC<StationaryShowcaseProps> = ({
     <ul className="flex flex-wrap md:flex-row flex-col">
       {showcasePosts.map((post, idx) => {
         return (
-          // TODO: this doesn't work because of the interpolated string
-          <li key={idx} className={`md:w-1/${columns} w-full`}>
+          <li key={idx} className={dynamicClassName(columns)}>
             <ShowcaseItem post={post} />
           </li>
         );
