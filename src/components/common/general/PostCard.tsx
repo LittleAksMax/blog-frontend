24051c32329a92583e6ea496dab7583d497d7dc5
David R <david2005.rosental@gmail.com>
2024-10-16 00:48:55 +0100

    PostCard now has some placeholder SVG file

--- src/components/common/general/PostCard.tsx
+++ src/components/common/general/PostCard.tsx
@@ -2,22 +2,37 @@ import { FC, useMemo } from 'react';
 import { Post } from '../../../sdk/types';
 import { DeleteButton, UpdateButton } from './buttons';
 import { useAuth } from '../../../contexts/auth';
+import { ChildrenProp } from '../../props';
 
-export interface PostCardProps {
+export interface PostCardProps extends ChildrenProp {
   post: Post;
   withButtons?: boolean;
   // TODO: background image
 }
 
-const PostCard: FC<PostCardProps> = ({ post, withButtons }: PostCardProps) => {
+const PostCard: FC<PostCardProps> = ({
+  post,
+  withButtons,
+  children,
+}: PostCardProps) => {
   const auth = useAuth();
   const url = useMemo(() => `/posts/${post.id}`, [post]);
   return (
-    <span>
-      <a href={url}>{post.title}</a>
-      {withButtons && auth.user && <UpdateButton />}
-      {withButtons && auth.user && <DeleteButton />}
-    </span>
+    <div>
+      <div>
+        <a href={url}>
+          <img src="/placeholder.svg" alt={`${post.title}`} />
+          <span>{post.title}</span>
+          <div>{children}</div>
+        </a>
+      </div>
+      {withButtons && auth.user && (
+        <div>
+          <UpdateButton />
+          <DeleteButton />
+        </div>
+      )}
+    </div>
   );
 };
 
